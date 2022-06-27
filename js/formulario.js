4cc203fb752187240b879543ef401450174642d7
andreemaoel <andre.carvalho.123@hotmail.com>
2022-06-27 10:14:56 -0300

    cpf

--- js/formulario.js
+++ js/formulario.js
@@ -65,7 +65,7 @@ const verifyCpf = (strCPF) => {
   var Soma;
   var Resto;
   Soma = 0;
-  if (strCPF == "00000000000") return false;
+  if (['00000000000', '11111111111', '22222222222', '33333333333', '44444444444', '55555555555', '66666666666', '77777777777', '88888888888', '99999999999'].includes(strCPF)) return false;
 
   for (let i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
   Resto = (Soma * 10) % 11;
